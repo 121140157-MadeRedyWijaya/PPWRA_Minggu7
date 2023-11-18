e516a6a0b6544ff241a759290235379d3b3ec6af
121140157-MadeRedyWijaya <148557749+121140157-MadeRedyWijaya@users.noreply.github.com>
2023-11-18 21:27:04 +0700

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,6 +2,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE DATABASE IF NOT EXISTS minggu7;
+USE minggu7;
+
 CREATE TABLE mahasiswa (
   nim bigint(10) NOT NULL,
   nama varchar(50) NOT NULL,
